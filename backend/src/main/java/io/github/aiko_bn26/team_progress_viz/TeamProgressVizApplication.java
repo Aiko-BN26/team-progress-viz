fd20441f4284fafb1ab4b9f49fff177b48a19ba9
YamaU10622 <tokuyama.yudai.jp@alumni.tsukuba.ac.jp>
2025-11-04 13:09:11 +0900

    開発環境の確認用

---
+++ backend/src/main/java/io/github/aiko_bn26/team_progress_viz/TeamProgressVizApplication.java
@@ -0,0 +1,13 @@
+package io.github.aiko_bn26.team_progress_viz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamProgressVizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamProgressVizApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/io/github/aiko_bn26/team_progress_viz/TeamProgressVizApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.aiko_bn26.team_progress_viz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamProgressVizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
