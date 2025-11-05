5e0fe1fe060a68e51178531083a23f70b96f6708
Shiba256 <153472750+Tomoyuki-Kawaguchi@users.noreply.github.com>
2025-11-05 23:01:01 +0900

    Configを追加

---
+++ backend/src/main/java/io/github/aikobn26/teamprogressviz/shared/config/SecurityConfig.java
@@ -0,0 +1,29 @@
+package io.github.aikobn26.teamprogressviz.shared.config;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(AbstractHttpConfigurer::disable)
+            .cors(withDefaults())
+            .authorizeHttpRequests(
+                auth -> auth.requestMatchers("/api/auth/**").permitAll().anyRequest().permitAll()
+            )
+            .formLogin(AbstractHttpConfigurer::disable)
+            .httpBasic(AbstractHttpConfigurer::disable)
+            .logout(AbstractHttpConfigurer::disable);
+        return http.build();
+    }
+}
