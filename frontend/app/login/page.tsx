cf53467d7145e46f24d30af889885a5bae99bce8
YamaU10622 <tokuyama.yudai.jp@alumni.tsukuba.ac.jp>
2025-11-06 13:27:03 +0900

    共有用

---
+++ frontend/app/login/page.tsx
@@ -0,0 +1,29 @@
+import router from "next/router";
+import { useEffect } from "react";
+
+export default function login() {
+  useEffect(() => {
+      const checkIsLogin = async () => {
+      try{
+        const res = await fetch("/api/auth/github/login");
+        const data = await res.json();
+
+        switch(res.status){
+          case 200:
+            const authorizationUrl = data.authorizationUrl;
+            router.push(authorizationUrl);
+            break;
+
+          case 500:
+            console.error(data.error);
+            break;
+
+          default:
+            break;
+        }
+      } catch(err){
+        console.error("fetch error:",err);
+      } };
+      checkIsLogin();
+    }, []);
+}
